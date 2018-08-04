946039bedd6f56c0c09339e1a065290ccaf80cbe
Edward Grundy <anax@hotmail.co.uk>
2018-08-04 09:46:23 +0100

    normalised interface to pca.py

--- pca.py
+++ pca.py
@@ -7,14 +7,20 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 import numpy as np
 from os.path import exists, join
 
-# pca
+CACHE_FILENAME = join ("data", "models", "pca.npz")
+
 def get_pca (pca_model_filename, x_train):
+  """build a pca model from data and save to disk.
+  if the file exists already, load and return that model instead
+  """
   if exists (pca_model_filename):
+    print ("loading model : '%s'" % pca_model_filename)
     data = np.load (pca_model_filename)
     U = data["U"]
     s = data["s"]
     V = data["V"]
   else:
+    mu = x_train.mean(axis=0)
     U,s,V = np.linalg.svd(x_train - mu, full_matrices=False)
 
     np.savez_compressed (pca_model_filename,
@@ -22,29 +28,32 @@ def get_pca (pca_model_filename, x_train):
 
   return U, s, V
 
-def get_pca_encodings (pca_model_filename, x_train, x_test):
-  pca_x_train = x_train.reshape (x_train.shape[0], x_train.shape[1]*x_train.shape[2])
-  pca_x_test = x_test.reshape (x_test.shape[0], x_test.shape[1]*x_test.shape[2])
-  mu = pca_x_train.mean(axis=0)
+def get_encodings (pca_model_filename, data):
+  """return the projection of data into the pca space
+  """
+  X = data.reshape (data.shape[0], np.prod (data.shape[1:]))
+  mu = X.mean (axis = 0)
 
-  U, s, V = get_pca (pca_model_filename, pca_x_train)
+  U, s, V = get_pca (CACHE_FILENAME, X)
 
-  Zpca = np.dot(pca_x_train - mu, V.transpose())
+  return np.dot (X - mu, V.transpose ())
 
-  return Zpca
+def get_reconstructions (data, test_data = None):
+  """return reconstructions of the data after projection into the
+      pca sub-space
+  """
+  X = data.reshape (data.shape[0], np.prod (data[1:]))
 
-def get_pca_reconstructions (x_train, x_test):
-  pca_model_filename = join ("data", "models", "pca.npz")
+  if test_data != None:
+    X_test = test_data.reshape (test_data.shape[0], np.prod (test_data[1:]))
 
-  pca_x_train = x_train.reshape (x_train.shape[0], x_train.shape[1]*x_train.shape[2])
-  pca_x_test = x_test.reshape (x_test.shape[0], x_test.shape[1]*x_test.shape[2])
-  mu = pca_x_train.mean(axis=0)
+  mu = X.mean (axis = 0)
 
-  U, s, V = get_pca (pca_model_filename, pca_x_train)
+  U, s, V = get_pca (CACHE_FILENAME, X)
 
-  Zpca = get_pca_encodings (x_train, x_test)
+  Zpca = get_pca_encodings (X, X_test)
   Rpca = np.dot(Zpca[:,:2], V[:2,:]) + mu    # reconstruction
-  err = np.sum((pca_x_train-Rpca)**2)/Rpca.shape[0]/Rpca.shape[1]
+  err = np.sum ((X-Rpca)**2)/Rpca.shape[0]/Rpca.shape[1]
   print('PCA reconstruction error with 2 PCs: ' + str(round(err,3)));
 
   return Rpca
